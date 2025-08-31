c8b0306be2b391d570e26450ea6a86a80ac97270
sai srujana <srujananagireddy82@gmail.com>
2025-08-31 22:58:37 +0530

    Add files via upload

---
+++ concatenation(+).py
@@ -0,0 +1,18 @@
+#concatenation(+):this opertor is used to concaternte value (combinng the values)
+v=(12,13,14,15)
+p=(16,17,18,19)
+l=v+p
+print(l)
+
+#repetion opeartor(*): this operator is used to repeat a given values
+s=(9,8,7,6)
+p=s*7
+print(p)
+
+#len(): this function is used to detemineted the lenght of the tuple
+r=(5,7,9,17,60,12,90)
+print (len(r))
+
+#count():this function is use to count the number of repeated values in a given sequence
+m=(23,24,25,26,27,28,29)
+print(m.count(24))
